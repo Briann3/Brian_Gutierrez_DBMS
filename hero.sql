30bfaa1c63a9c5ea18fbe54038724e73d5ab62b9
user name mo dito <email sa github dito>
2023-11-08 00:43:33 +0800

    feat added update and delete

--- hero.sql
+++ hero.sql
@@ -1,2 +1,7 @@
-ALTER TABLE public.item
-ADD item_price DECIMAL(10, 2);
+UPDATE public.hero
+SET is_active = false
+WHERE hero_id = 1;
+
+-- Delete the item associated with hero 1
+DELETE FROM public_heroitem
+WHERE hero_id = 1;
